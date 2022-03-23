dd441ac71f52467e2f4e5ee21629c52527609d8b
MGILLES <maxence.gilles@ynov.com>
2022-03-23 11:46:58 +0100

    cyber

--- ydays-cyber/install.sh
+++ ydays-cyber/install.sh
@@ -11,4 +11,14 @@ apt install whois -y
 apt install beef-xss -y
 apt install metasploit-framework -y
 apt install sqlmap -y
-apt install nslookup -y 
+apt install dnsutils -y
+echo "
+<background>
+  <static>
+    <duration>8640000.0</duration>
+    <file>
+      <size width=""1920"" height=""1080"">/path/to/wallpaper.jpg</size>
+    </file>
+  </static>
+</background>
+" >> /usr/share/images/desktop-base/lockscreen.xml 
